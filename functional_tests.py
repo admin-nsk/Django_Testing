3087d40335c24999421eb1127aebf0de46975dd2
Alexey Petrov <adm_sib@mail.ru>
2021-10-13 15:06:46 +0700

    write test class

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,25 @@
+import os
+
 from selenium import webdriver
+import unittest
 
 browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
 
-assert 'install worked' in browser.title
+class NewVistorTest(unittest.TestCase):
+    ''' Тест нового посетителя'''
+
+    def setUp(self) -> None:
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self) -> None:
+        self.browser.quit()
+        self.browser.close()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        self.browser.get('http://localhost:8000')
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Закончить тест!')
+
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
